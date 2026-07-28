53b4f67415e40c13a6c8d40bcfda86190411c64a
EthernalBuddha <infogermanparts@gmail.com>
2026-07-28 15:56:48 +0400

    deploy: add Foundry deploy script, arc rpc endpoint, deployed SaveFactory 0xaa05Fe34

---
+++ script/Deploy.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/Save.sol";
+
+contract DeployFactory is Script {
+    function run() external returns (address factory) {
+        vm.startBroadcast();
+        SaveFactory f = new SaveFactory();
+        vm.stopBroadcast();
+
+        factory = address(f);
+        console2.log("SaveFactory deployed at:", factory);
+    }
+}
